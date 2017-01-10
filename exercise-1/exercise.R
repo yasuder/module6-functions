d9d414d1ec8cbe0130e51fce533504012aae11eb
Erika Yasuda <eri.yasuda222@gmail.com>
2017-01-10 15:17:43 -0800

    Add complete exercise 1 and partially complete exercise 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,15 +2,22 @@
 
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-
+AddThree <- function(num) {
+  three.greater <- num + 3
+  return(three.greater)
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
-
+ImperialToMetric <- function(feet, inches) {
+  meters <- (feet * 30 + inches * 2.54) / 100
+  return(meters)
+}
 
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
+height.in.meters <- ImperialToMetric(5, 5)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,10 +3,15 @@
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-
+CompareLength <- function(string1, string2) {
+  length1 <- nchar(string1)
+  length2 <- nchar(string2)
+  diff <- abs(length1 - length2)
+  return(diff)
+}
 
 # Pass two strings of different lengths to your `CompareLength` function
-
+CompareLength("Erika", "Homework")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
