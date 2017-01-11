9ab0485a7b256b640be33c9f583e7eac60e7b333
Erika Yasuda <eri.yasuda222@gmail.com>
2017-01-11 12:26:03 -0800

    Add complete exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,16 +6,24 @@
 CompareLength <- function(string1, string2) {
   length1 <- nchar(string1)
   length2 <- nchar(string2)
-  diff <- abs(length1 - length2)
+  diff <- abs(length2 - length1)
   return(diff)
 }
 
 # Pass two strings of different lengths to your `CompareLength` function
-CompareLength("Erika", "Homework")
+three <- CompareLength("Erika", "Homework")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
-
+DescribeDifference <- function(string1, string2) {
+  diff <- CompareLength(string1, string2)
+  if(nchar(string1) > nchar(string2)) {
+    phrase <- paste("Your first string is longer by", diff, "characters")
+  } else {
+    phrase <- paste("Your second string is longer by", diff, "characters")
+  }
+}
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+statement <- DescribeDifference("Erika", "Homework")
